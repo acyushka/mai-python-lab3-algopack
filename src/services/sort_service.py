7e3d4ad6dcc00ea6cc52a062503598bc8ac24eb9
acyushka <izotovarsenii3005@gmail.com>
2025-11-29 10:41:29 +0300

    Add: docstrings for sorts

--- src/services/sort_service.py
+++ src/services/sort_service.py
@@ -8,6 +8,9 @@ T = TypeVar('T')
 @dataclass
 class SortingService(Generic[T]):
     def bubble_sort(self, a: list[int]) -> list[int]:
+        """
+        Обычная реализация бабл сорт: ходим по каждым двум рядом стоящим элементам в списке, сравниваем их между собой
+        """
         n = len(a)
 
         for i in range(n - 1):
@@ -17,6 +20,12 @@ class SortingService(Generic[T]):
         return a
 
     def quick_sort(self, a: list[int]) -> list[int]:
+        """
+        Задаем сначала базоый случай рекурсии.
+        Выбираем опорное число pivot: я беру середину.
+        Делим числа на 3 категории относительно pivot: меньшие, равные, большие.
+        Вызываем рекурсию, пока не доберемся до опорного случая.
+        """
         n = len(a)
         if n < 2:
             return a
@@ -30,12 +39,17 @@ class SortingService(Generic[T]):
         return self.quick_sort(left) + middle + self.quick_sort(right)
 
     def counting_sort(self, a: list[int]) -> list[int]:
+        """
+        Делаем список counter из константного кол-ва нулей. Это счетчик каждого числа встреченного в заданном списке.
+        Проходимся по списку и отмечаем в counter встреченные числа, инкрементируем счетчики на позициях counter, равных встреченным числам.
+        Проходимся по counter еще раз, если какое-то число там != 0: ставим его в нужную позицию в исходном списке. Готово
+        """
         if not a:
             return []
 
         n = len(a)
 
-        counter: list[int] = [0]*10000
+        counter: list[int] = [0] * 10000
 
         for i in range(n):
             counter[a[i]] += 1
@@ -49,11 +63,15 @@ class SortingService(Generic[T]):
         return a
 
     def radix_sort(self, a: list[int], base: int = 10) -> list[int]:
+        """
+        Проходимся по разрядам чисел, для этого сначала по максимальному числу находим максимальный разряд.
+        Потом собственно по разрядам идем, от меньшего к большему. На каждом проходе делаем counting sort, делая вспомогательный список из 0 длиной в заданную base.
+        """
         if not a:
             return []
 
         n = len(a)
-        b = [0] * n
+        b = [0] * n # начальный временный массив
 
         max_val = max(a)
         m = 1
@@ -63,28 +81,33 @@ class SortingService(Generic[T]):
             temp //= base
 
         for i in range(m):
-            c = [0] * base
+            counter = [0] * base
 
             for j in range(n):
                 digit = (a[j] // (base ** i)) % base
-                c[digit] += 1
+                counter[digit] += 1
 
             count = 0
             for j in range(base):
-                temp_count = c[j]
-                c[j] = count
+                temp_count = counter[j]
+                counter[j] = count
                 count += temp_count
 
             for j in range(n):
                 digit = (a[j] // (base ** i)) % base
-                b[c[digit]] = a[j]
-                c[digit] += 1
+                b[counter[digit]] = a[j]
+                counter[digit] += 1
 
             a, b = b, a
 
         return a
 
     def bucket_sort(self, a: list[float], buckets: int | None = None) -> list[float]:
+        """
+        Задаем сначала базовый случай рекурсии. Потом считаем кол-во корзин, если не задано.
+        Делаем список корзин. Проходимся по каждому элементу заданного списка. Нормализуем каждый элемент.
+        Потом сортируем каждую корзину рекурсивно.
+        """
         try:
             if len(a) < 2:
                 return a
@@ -115,11 +138,15 @@ class SortingService(Generic[T]):
         except RecursionError as e:
             raise RecursionError("Вы закинули во входные данные фигню, возникла бесконечная рекурсия")
 
-
     def heap_sort(self, a: list[int]) -> list[int]:
+        """
+        Первый цикл: проходимся по всем узлам списка и делаем кучу с помощью heapify.
+        Второй цикл: выставляем итерационно верхушки кучи, при этом пересчитываем кучу.
+        """
         n = len(a)
 
         def heapify(n: int, i: int) -> None:
+            """Функция выстраивания кучи"""
             largest = i
             left = 2 * i + 1
             right = 2 * i + 2
@@ -134,7 +161,7 @@ class SortingService(Generic[T]):
                 heapify(n, largest)
 
         for i in range(n // 2 - 1, -1, -1):
-            heapify(n,i)
+            heapify(n, i)
 
         for i in range(n - 1, 0, -1):
             a[0], a[i] = a[i], a[0]
